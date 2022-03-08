eda0c3956ab2a6ebdb7dec112e8ade55002d4837
m00p1ng <mongkonchai4412@gmail.com>
2022-03-08 11:44:11 +0700

    add typescript

---
+++ 05-react-native-desktop/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+interface IProps {}
+
+export const App = (props: IProps) => {
+  return (
+    <View>
+      <Text>Hello from typescript</Text>
+    </View>
+  )
+}
